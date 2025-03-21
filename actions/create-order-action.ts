81d002fdb2afe16db07485f3264c77ce16d714c9
lemus <lemus777@gmail.com>
2025-03-21 12:04:06 +0100

    validating data with zod in the server

--- actions/create-order-action.ts
+++ actions/create-order-action.ts
@@ -1,5 +1,18 @@
 "use server"
 
-export async function createOrder() {
-  console.log('Desde create-order-action.ts')
+import { OrderSchema } from "@/src/schema"
+
+export async function createOrder(data: unknown) {
+  const result = OrderSchema.safeParse(data)
+
+  if(!result.success) {
+    return {
+      errors: result.error.issues
+    }
+  }
+  try {
+    
+  } catch (error) {
+    console.log(error)
+  }
 }
--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -11,7 +11,7 @@ export default function OrderSummary() {
   const order = useStore((state) => state.order)
   const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
-  const handleCreateOrder = (formData : FormData) => {
+  const handleCreateOrder = async (formData : FormData) => {
     const data = {
       name: formData.get('name')
     }
@@ -20,9 +20,14 @@ export default function OrderSummary() {
       result.error.issues.forEach((issue) => {
         toast.error(issue.message)
       })
+      return
     }
-    return
-    createOrder()
+    const response = await createOrder(data)
+    if(response?.errors) {
+      response.errors.forEach((issue) => {
+        toast.error(issue.message)
+      })
+    }
   }
 
   return (
