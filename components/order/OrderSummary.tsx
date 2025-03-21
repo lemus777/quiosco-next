7b6e4d5c6b466df489e9a0fb9065bd7d50df1646
lemus <lemus777@gmail.com>
2025-03-21 11:28:33 +0100

    recovering data from form with formData

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -9,8 +9,8 @@ export default function OrderSummary() {
   const order = useStore((state) => state.order)
   const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
-  const handleCreateOrder = () => {
-    console.log('Desde handleCreateOrder')
+  const handleCreateOrder = (formData : FormData) => {
+    console.log(formData.get('name'))
     createOrder()
   }
 
@@ -33,6 +33,12 @@ export default function OrderSummary() {
             className="w-full mt-10 space-y-5"
             action={handleCreateOrder}
           >
+            <input
+              type="text"
+              placeholder="Tu nombre"
+              className="bg-white border border-gray-100 p-2 w-full"
+              name="name"
+            />
             <input
               type="submit"
               className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
