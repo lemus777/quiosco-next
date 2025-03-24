d8ab68adefdcc677c3645ab01bf860aaebb5ebfd
lemus <lemus777@gmail.com>
2025-03-24 11:46:37 +0100

    complete order action created

--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -1,3 +1,4 @@
+import { completeOrder } from "@/actions/complete-order-action";
 import { OrderWithProducts } from "@/src/types";
 import { formatCurrency } from "@/src/utils";
 
@@ -6,7 +7,6 @@ type OrderCardProps = {
 }
 
 export default function OrderCard({ order }: OrderCardProps) {
-  console.log(order)
   return (
     <section
       aria-labelledby="summary-heading"
@@ -34,7 +34,7 @@ export default function OrderCard({ order }: OrderCardProps) {
         </div>
       </dl>
 
-      <form>
+      <form action={completeOrder}>
         <input
           type="submit"
           className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
