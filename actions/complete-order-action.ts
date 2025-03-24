7998114542ee281f568693a541440b5d2594ec84
lemus <lemus777@gmail.com>
2025-03-24 17:33:11 +0100

    completing orders and validation

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,5 +1,27 @@
 "use server"
 
-export async function completeOrder() {
-  console.log('Desde completeOrder')
+import { prisma } from "@/src/lib/prisma"
+import { OrderIdSchema } from "@/src/schema"
+
+export async function completeOrder(formData: FormData) {
+  const data = {
+    orderId: formData.get('order_id')
+  }
+  const result = OrderIdSchema.safeParse(data)
+
+  if(result.success) {
+    try {
+      await prisma.order.update({
+        where: {
+          id: result.data.orderId
+        },
+        data: {
+          status: true,
+          orderReadyAt: new Date(Date.now())
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
