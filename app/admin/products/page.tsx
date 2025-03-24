b759800d78ac759ef746b4a7fa154f54c65c7125
lemus <lemus777@gmail.com>
2025-03-24 19:01:18 +0100

    adding category to the table

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -3,9 +3,15 @@ import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
 async function getProducts() {
-  const products = await prisma.product.findMany()
+  const products = await prisma.product.findMany({
+    include: {
+      category: true
+    }
+  })
   return products
 }
+
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
 
 export default async function ProductsPage() {
   const products = await getProducts()
