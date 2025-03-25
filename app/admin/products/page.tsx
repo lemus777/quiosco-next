a26ece6f1bf6720ebd898019b89ac209a7b6e583
lemus <lemus777@gmail.com>
2025-03-25 12:22:48 +0100

    router for navigate in pagination created

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,11 @@
+import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+
+async function productCount() {
+  return await prisma.product.count()
+}
 
 async function getProducts(page: number, pageSize: number) {
   const skip = (page - 1) * pageSize
@@ -20,13 +25,19 @@ export default async function ProductsPage({searchParams} : {searchParams: {page
   const page = +searchParams.page || 1
   const pageSize = 10
   
-  const products = await getProducts(page, pageSize)
+  const productsData = getProducts(page, pageSize)
+  const totalProductsData = productCount()
+  const [products, totalProducts] = await Promise.all([productsData, totalProductsData])
+  console.log(totalProducts)
 
   return (
     <>
       <Heading>Administrar productos</Heading>
       <ProductTable
         products={products}
+      />
+      <ProductsPagination
+        page={page}
       />
     </>
   )
