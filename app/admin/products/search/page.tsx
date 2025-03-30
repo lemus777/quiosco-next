64005248a5f3cee9903cae4beaddb70e49bebd66
lemus <lemus777@gmail.com>
2025-03-30 16:33:49 +0200

    getting the search value

---
+++ app/admin/products/search/page.tsx
@@ -0,0 +1,10 @@
+import Heading from "@/components/ui/Heading"
+
+export default async function SearchPage({searchParams} : {searchParams: {search: string}}) {
+  console.log(searchParams.search)
+  return (
+    <>
+      <Heading>Resultados de búsqueda</Heading>
+    </>
+  )
+}
