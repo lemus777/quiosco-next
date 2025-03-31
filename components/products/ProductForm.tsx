1ec38edf7e920b6961c494caa1f7c00cf8c8eaf5
lemus <lemus777@gmail.com>
2025-03-31 13:16:35 +0200

    showing categories in the form

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -1,4 +1,13 @@
+import { prisma } from "@/src/lib/prisma";
+
+async function getCategories() {
+  return await prisma.category.findMany()
+}
+
 export default async function ProductForm() {
+  const categories = await getCategories()
+  console.log(categories)
+
   return (
     <>
       <div className="space-y-2">
@@ -36,6 +45,12 @@ export default async function ProductForm() {
           name="categoryId"
         >
           <option value="">-- Seleccione --</option>
+          {categories.map(category => (
+            <option
+              key={category.id}
+              value={category.id}
+            >{category.name}</option>
+          ))}
         </select>
       </div>
     </>
