e60ae5b21dc3d8a5433b783336ae75669b06cbb0
lemus <lemus777@gmail.com>
2025-04-01 12:59:54 +0200

    problem mixing server and client components

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,10 +1,16 @@
+"use client"
+
 import ProductForm from "./ProductForm";
 
 export default function AddProductForm() {
+  const handleSubmit = async (formData : FormData) => {
+    console.log('desde handleSubmit')
+  }
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
       <form
         className="space-y-5"
+        action={handleSubmit}
       >
         <ProductForm />
         <input
