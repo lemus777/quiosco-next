3d511860f620c0e7668c1c5b9de8ac5aa57a5563
lemus <lemus777@gmail.com>
2025-04-03 13:01:57 +0200

    validating new products

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,9 +1,25 @@
 "use client"
 
+import { ProductSchema } from "@/src/schema"
+import { toast } from "react-toastify"
+
 export default function AddProductForm({children}: {children : React.ReactNode}) {
   const handleSubmit = async (formData : FormData) => {
-    console.log('desde handleSubmit')
+    const data = {
+      name: formData.get('name'),
+      price: formData.get('price'),
+      categoryId: formData.get('categoryId')
+    }
+    const result = ProductSchema.safeParse(data)
+    if(!result.success) {
+      result.error.issues.forEach(issue => {
+        toast.error(issue.message)
+      })
+      return
+    }
+    console.log(result.data)
   }
+
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
       <form
