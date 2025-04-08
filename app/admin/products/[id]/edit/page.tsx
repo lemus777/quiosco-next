cab2e2ef236340f4de829f799d58388802fb80ca
lemus <lemus777@gmail.com>
2025-04-08 13:43:38 +0200

    getting the product to edit

---
+++ app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,23 @@
+import { prisma } from "@/src/lib/prisma"
+import { notFound } from "next/navigation"
+
+async function getProductById(id: number) {
+  const product = await prisma.product.findUnique({
+    where: {
+      id
+    }
+  })
+  if(!product) {
+    notFound()
+  }
+  return product
+}
+
+export default async function EditProductsPage({params}: {params: {id: string}}) {
+  const product = await getProductById(+params.id)
+  console.log(product)
+
+  return (
+    <div>page</div>
+  )
+}
