52521b03a31e78b862c7be74f82d47850b053402
lemus <lemus777@gmail.com>
2025-04-12 18:38:19 +0200

    endpoint API for ready orders screen

---
+++ app/orders/page.tsx
@@ -0,0 +1,10 @@
+import Logo from "@/components/ui/Logo";
+
+export default function OrdersPage() {
+  return (
+    <>
+      <h1 className="text-center mt-20 text-6xl font-black">Órdenes listas</h1>
+      <Logo />
+    </>
+  )
+}
