2e6e455e8422d241c6588be6dcbb61f7c9e70bfd
lemus <lemus777@gmail.com>
2025-04-13 13:40:47 +0200

    fix: force-dynamic

--- app/orders/api/route.tsx
+++ app/orders/api/route.tsx
@@ -1,8 +1,10 @@
 import { prisma } from "@/src/lib/prisma";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   const orders = await prisma.order.findMany({
-    take: 5,
+    take: 6,
     where: {
       orderReadyAt: {
         not: null
